a954280b13aa7bc9959f326a0abfbcafb7c7f900
Johannes Donath <johannesd@evil-co.com>
2014-07-08 00:02:47 +0200

    Added TimeUpdatePacket definition.

---
+++ common/src/main/java/org/evilco/mc/protocol/common/packet/client/TimeUpdatePacket.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014 Johannes Donath <johannesd@evil-co.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evilco.mc.protocol.common.packet.client;
+
+import io.netty.buffer.ByteBuf;
+import org.evilco.mc.protocol.common.packet.IPacket;
+
+/**
+ * Represents the time update packet.
+ */
+public class TimeUpdatePacket implements IPacket {
+
+	/**
+	 * Stores the world age.
+	 */
+	protected long age;
+
+	/**
+	 * The current region time.
+	 */
+	protected long time;
+
+	/**
+	 * Constructs a new TimeUpdatePacket instance.
+	 * @param age The world age.
+	 * @param time The region time.
+	 */
+	public TimeUpdatePacket (long age, long time) {
+		this.age = age;
+		this.time = time;
+	}
+
+	/**
+	 * Constructs a new TimeUpdatePacket instance.
+	 * @param buffer The packet buffer.
+	 */
+	public TimeUpdatePacket (ByteBuf buffer) {
+		this.age = buffer.readLong ();
+		this.time = buffer.readLong ();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void write (ByteBuf out) {
+		out.writeLong (this.age);
+		out.writeLong (this.time);
+	}
+}
